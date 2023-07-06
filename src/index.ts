ff3d0d972f8dc52ce73d79df8f309aa12517638c
Rashad <rashad.suleymanov83@gmail.com>
2023-07-07 00:06:13 +0400

    [WIP] CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,79 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
+import "dotenv/config";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+console.log(process.env.NAME);
+
+const products = [
+  { id: 1, title: "tomato" },
+  { id: 2, title: "apple" },
+  { id: 3, title: "pear" },
+];
+const addresses = [
+  {
+    id: 1,
+    street: "123 Main St",
+    city: "Cityville",
+    state: "State",
+    country: "Country",
+    postalCode: "12345",
+  },
+  {
+    id: 2,
+    street: "456 Elm St",
+    city: "Townsville",
+    state: "State",
+    country: "Country",
+    postalCode: "67890",
+  },
+  {
+    id: 3,
+    street: "789 Oak St",
+    city: "Villagetown",
+    state: "State",
+    country: "Country",
+    postalCode: "54321",
+  },
+];
 
 app.get("/", (req: Request, res: Response) => {
-  const hello = "Hello world !!!";
+  const hello = "Hello world 23";
   res.send(hello);
+});
+
+app.get("/products", (req: Request, res: Response) => {
+  res.send(products);
+});
+
+app.get("/addresses", (req: Request, res: Response) => {
+  res.send(addresses);
+});
+
+app.get("/products/:productTitle", (req: Request, res: Response) => {
+  const product = products.find(
+    (product) => product.title === req.params.productTitle
+  );
+
+  if (!product) {
+    res.send(404);
+    return;
+  }
+
+  res.send(product);
+});
+
+app.get("/adresses/:id", (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  const address = addresses.find((address) => address.id === id);
+
+  if (!address) {
+    res.send(404);
+    return;
+  }
+
+  res.send(address);
 });
 
 app.listen(port, () => {
