da21af37013ca6e76141d99664cc617d5ebc1d10
Rashad <rashad.suleymanov83@gmail.com>
2023-07-09 22:24:30 +0400

    Add Product Routers

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
 import bodyParser from "body-parser";
+import { producstRouter } from "./routers/products-router";
 
 const app = express();
 app.use(bodyParser.json());
+app.use('/products', producstRouter)
 
 const port = process.env.PORT || 3000;
-console.log(process.env.NAME);
-
-const products = [
-  { id: 1, title: "tomato" },
-  { id: 2, title: "apple" },
-  { id: 3, title: "pear" },
-];
 
 const addresses = [
   {
@@ -40,58 +35,6 @@ const addresses = [
     postalCode: "54321",
   },
 ];
-
-app.get("/products", (req: Request, res: Response) => {
-  res.send(products);
-});
-
-app.post("/products", (req: Request, res: Response) => {
-  const newProduct = { id: Number(new Date()), title: req.body.title };
-  products.push(newProduct);
-
-  res.status(201).send(newProduct);
-});
-
-app.get("/products/:productTitle", (req: Request, res: Response) => {
-  const product = products.find(
-    (product) => product.title === req.params.productTitle
-  );
-
-  if (!product) {
-    res.send(404);
-    return;
-  }
-
-  res.send(product);
-});
-
-
-app.put("/products/:id", (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id === id) {
-      
-      products.slice(i, 1);
-      res.send(201);
-      return;
-    }
-  }
-
-  res.send(404);
-});
-
-app.delete("/products/:id", (req: Request, res: Response) => {
-  const id = Number(req.params.id);
-  for (let i = 0; i < products.length; i++) {
-    if (products[i].id === id) {
-      products.slice(i, 1);
-      res.send(201);
-      return;
-    }
-  }
-
-  res.send(404);
-});
 
 app.get("/adresses/:id", (req: Request, res: Response) => {
   const id = Number(req.params.id);
---
+++ src/routers/products-router.ts
@@ -0,0 +1,61 @@
+import express, { Request, Response, Router } from "express";
+
+// export const producstRouter = Router
+export const producstRouter = express.Router();
+
+const products = [
+  { id: 1, title: "tomato" },
+  { id: 2, title: "apple" },
+  { id: 3, title: "pear" },
+];
+
+producstRouter.get("/", (req: Request, res: Response) => {
+  res.send(products);
+});
+
+producstRouter.post("/", (req: Request, res: Response) => {
+  const newProduct = { id: Number(new Date()), title: req.body.title };
+  products.push(newProduct);
+
+  res.status(201).send(newProduct);
+});
+
+producstRouter.get("/:productTitle", (req: Request, res: Response) => {
+  const product = products.find(
+    (product) => product.title === req.params.productTitle
+  );
+
+  if (!product) {
+    res.send(404);
+    return;
+  }
+
+  res.send(product);
+});
+
+producstRouter.put("/:id", (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === id) {
+      
+      products.slice(i, 1);
+      res.send(201);
+      return;
+    }
+  }
+
+  res.send(404);
+});
+
+producstRouter.delete("/:id", (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === id) {
+      products.slice(i, 1);
+      res.send(201);
+      return;
+    }
+  }
+
+  res.send(404);
+});
